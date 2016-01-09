156783129338fe804bf3a00290c8b90897b40182
Pierre Rudloff <contact@rudloff.pro>
2016-01-09 19:30:52 +0100

    Initial import

---
+++ PageContributors.i18n.magic.php
@@ -0,0 +1,7 @@
+<?php
+//Magic word id : MAG_PageContributorss
+
+$magicWords = array();
+$magicWords['en'] = array(
+	'PageContributors' => array( 0, 'PageContributors' , 'Listedescontributeurs'),
+);
